61007c7e9c974bcc30e1df76866faa0490352003
Somendra Paul <somendrapaul7@gmail.com>
2025-02-02 18:37:50 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import BanglaHindiTranscriptor from "../bangla-hindi-transcriptor"
-
-export default function SyntheticV0PageForDeployment() {
-  return <BanglaHindiTranscriptor />
-}
